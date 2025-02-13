fb689342262c9720aad13f068b56a3f426896413
jirak443 <jiranon.k@entronica.in.th>
2025-02-13 17:57:01 +0700

    backup

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,45 @@
+"use client";
 
-
-import { projects } from "@/config/projects";
-import {Card, CardHeader, CardBody, CardFooter} from "@heroui/card";
-import {Divider} from "@heroui/divider"
-import {Image} from "@heroui/image"
-import { Link } from "@heroui/link";
-
+import { Pagination } from "@heroui/pagination";
+import { useState, useEffect } from "react";
+import { stacks } from '@/config/stack';
+import Stack from "@/components/stacks";
+import Worker from "@/components/worker";
+import { Divider } from "@heroui/divider";
 export default function Home() {
-  return ( <div className="flex justify-between">
-    <section className="flex flex-col m-10">
-      <h1 className="font-bold text-7xl">Hello I'm</h1>
-      <br></br>
-      <h2 className="text-3xl">Jiranon Khemklad</h2>
-      <h2>software engineer</h2>
-    </section>
-    <section className="flex flex-col m-5">
-    <Card className="max-w-[400px]">
-      <CardHeader className="flex gap-3">
-        <Image
-          alt="heroui logo"
-          height={40}
-          radius="sm"
-          src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
-          width={40}
-        />
-        <div className="flex flex-col">
-          <p className="text-md">HeroUI</p>
-          <p className="text-small text-default-500">heroui.com</p>
-        </div>
-      </CardHeader>
-      <Divider />
-      <CardBody>
-        <p>Make beautiful websites regardless of your design experience.</p>
-      </CardBody>
-      <Divider />
-      <CardFooter>
-        <Link isExternal showAnchorIcon href="https://github.com/heroui-inc/heroui">
-          Visit source code on GitHub.
-        </Link>
-      </CardFooter>
-    </Card>
-    </section>
+  const [total, setTotal] = useState(stacks.length);
+  const [current, setCurrent] = useState(1); // เริ่มต้นจากหน้า 1
+
+  // การคำนวณการแสดงข้อมูลตามหน้า
+  const itemsPerPage = 1; // กำหนดจำนวนโปรเจกต์ที่จะแสดงในแต่ละหน้า
+  const currentPageItems = stacks.slice(
+    (current - 1) * itemsPerPage,
+    current * itemsPerPage
+  );
+
+
+
+  return (
+    <>
+    <div className="flex justify-between">
+      <section className="flex flex-col m-10">
+        <h1 className="font-bold text-7xl">Hello I'm</h1>
+        <h2 className="text-3xl">Jiranon Khemklad</h2>
+        <h2>Software Engineer</h2>
+      </section>
+
+      <section className="flex m-5">
+      <Stack></Stack>
+       </section>
+      
+     
+      
+     
     </div>
+    <Divider></Divider>
+     <section className="pt-10">
+     <Worker></Worker>
+     </section>
+     </>
   );
 }
---
+++ components/worker.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Image } from '@heroui/image'
+import { works } from '@/config/work';
+import {Accordion, AccordionItem} from "@heroui/accordion";
+import { Link } from '@heroui/link';
+function Worker() {
+    const workerIcon = ""
+  return (
+    <>
+    <div>
+        {works.map(((work,index) => (
+          <div>
+          <Accordion key={index}  variant="splitted">
+          <AccordionItem aria-label={work.projectName} title={work.projectName}>
+          <Link isExternal showAnchorIcon href={work.link} className='m-2 '>
+          Visit source code on GitHub.
+        </Link>
+        <br></br>
+        <p className='whitespace-pre-line'>
+            {work.description}
+            </p>
+          </AccordionItem>
+          </Accordion>
+          {/* <div className='flex flex-row'>
+           {
+            work.images.map(((image,index) => (
+              <Image
+              key={index}
+              className='p-2'
+      alt="HeroUI hero Image"
+      src={image}
+      width={300}
+    />
+            )))
+           }
+           </div> */}
+          </div>
+          
+          
+        )))
+          
+        }
+    </div>
+
+    </>
+  )
+}
+
+Worker.propTypes = {}
+
+export default Worker
